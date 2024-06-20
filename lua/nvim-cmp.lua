fe2cc0b90e3125626e0a69f03bc4b8ef37649bac
Il-Capitano <52455591+Il-Capitano@users.noreply.github.com>
2024-06-20 22:53:32 +0200

    small improvements to `nvim-cmp` setup

--- lua/nvim-cmp.lua
+++ lua/nvim-cmp.lua
@@ -12,9 +12,9 @@ cmp.setup({
 		['<C-f>'] = cmp.mapping.scroll_docs(4),
 		['<C-Space>'] = cmp.mapping.complete(),
 		['<C-e>'] = cmp.mapping.abort(),
-		['<CR>'] = cmp.mapping.confirm({ select = true }),
+		['<CR>'] = cmp.mapping.confirm({ select = false }),
 		['<Tab>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-		['<C-Tab>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+		['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
 	}),
 	sources = cmp.config.sources(
 		{{ name = 'nvim_lsp' }},
